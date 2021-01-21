fda5f40d6ca0fbc516c8e8b65233c29d87c6665b
ardianjatip <ardian.jati@ui.ac.id>
2021-01-22 05:38:09 +0700

    runtime permission

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/ardian/elink/medicine/MedicineActivity.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/ardian/elink/medicine/MedicineActivity.java
@@ -1,15 +1,23 @@
 package id.ac.ui.cs.mobileprogramming.ardian.elink.medicine;
 
 
+import android.Manifest;
 import android.app.NotificationManager;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import com.google.android.material.appbar.AppBarLayout;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.view.ViewCompat;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -19,6 +27,7 @@ import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import id.ac.ui.cs.mobileprogramming.ardian.elink.Injection;
 import id.ac.ui.cs.mobileprogramming.ardian.elink.R;
@@ -38,6 +47,7 @@ import butterknife.OnClick;
 
 public class MedicineActivity extends AppCompatActivity {
 
+    private int LOCATION_PERMISSION_CODE = 1;
 
     @BindView(R.id.compactcalendar_view)
     CompactCalendarView mCompactCalendarView;
@@ -82,6 +92,12 @@ public class MedicineActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         setSupportActionBar(toolbar);
 
+        if((ContextCompat.checkSelfPermission(MedicineActivity.this, Manifest.permission.ACCESS_MEDIA_LOCATION)) ==
+                PackageManager.PERMISSION_GRANTED){
+            Toast.makeText(MedicineActivity.this, "You have already granted this permission", Toast.LENGTH_SHORT).show();
+        }else {
+            requestLocationPermission();
+        }
 
         mCompactCalendarView.setLocale(TimeZone.getDefault(), /*Locale.getDefault()*/Locale.ENGLISH);
 
@@ -156,5 +172,39 @@ public class MedicineActivity extends AppCompatActivity {
 
         isExpanded = !isExpanded;
         appBarLayout.setExpanded(isExpanded, true);
+    }
+
+    private void requestLocationPermission(){
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_MEDIA_LOCATION)){
+            new AlertDialog.Builder(this)
+                    .setTitle("Permission needed")
+                    .setMessage("This permission is needed because this app can track you")
+                    .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(MedicineActivity.this, new String[]{Manifest.permission.ACCESS_MEDIA_LOCATION},LOCATION_PERMISSION_CODE);
+                        }
+                    })
+                    .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    }).create().show();
+        }else{
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_MEDIA_LOCATION}, LOCATION_PERMISSION_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == LOCATION_PERMISSION_CODE){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "Permission Granted",Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(this, "Permission Denied",Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
